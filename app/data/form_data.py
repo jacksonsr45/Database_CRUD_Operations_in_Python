f69cd447a762d74a0a7523a880ac143529d390c4
jacksonsr45 <31199441+jacksonsr45@users.noreply.github.com>
2020-01-27 13:18:00 -0300

    Jackson
    Up Model CRUD

--- app/data/form_data.py
+++ app/data/form_data.py
@@ -1,8 +1,10 @@
 __author__ = 'jacksonsr45@gmail.com'
 
+from app.model.crud_model import *
+
 class data(object):
     def __init__(self):
-        pass
+        Create()
 
     def View_frame(self):
         pass
