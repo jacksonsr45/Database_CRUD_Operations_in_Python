22125eb9a491c11c96b193eb5a50155e9c1fd6e2
Jackson <jacksonsr45@gmail.com>
2020-02-19 12:31:16 -0300

    remove file form_data

--- app/main.py
+++ app/main.py
@@ -2,8 +2,11 @@ __author__ = 'jacksonsr45@gmail.com'
 
 import tkinter
 from app.data.form_crud import Crude
+from app.model.crud_model import Create
+
 
 class Main:
+    Create()
     root = tkinter.Tk()
     Crude(root)
     root.mainloop()
